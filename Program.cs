6134f036b903987b111af3a4aca3419426421501
luki981259 <59644074+luki981259@users.noreply.github.com>
2020-10-28 16:08:57 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace serwerTCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BibliotekKlas.SerwerTCP serwer = new BibliotekKlas.SerwerTCP();
+            serwer.Start();
+         
+        }
+    }
+}
